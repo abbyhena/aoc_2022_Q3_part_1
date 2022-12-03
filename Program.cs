d42cbd07c52bb3bb8ef615b837ff79ffabd01bba
abbyhena <abinayamuralidhara@email.grcc.edu>
2022-12-03 18:12:35 -0500

    part_3_soln
    part 3 first part solution

---
+++ Program.cs
@@ -0,0 +1,248 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+ var rucksackList = new List<string>(); 
+ var commonList = new List<char>();
+ var commonElem = new StringBuilder();
+ int total = 0;
+
+  foreach (string line in System.IO.File.ReadLines(@"/Users/dd/aoc_2022_Q3_part_1/input.txt"))
+        {  
+            rucksackList.Add(line); 
+        }  
+   for(int i = 0; i < rucksackList.Count(); i++)
+   {
+   var split = (rucksackList[i].Length/2);
+   string firsthalf = rucksackList[i].Substring(0, rucksackList[i].Length/2);
+   string secondhalf = rucksackList[i].Substring(split,rucksackList[i].Length/2);
+   char[] first = firsthalf.ToCharArray();
+   char[] second = secondhalf.ToCharArray();
+   var showIntersect = first.Intersect(second);
+   Console.WriteLine("Intersect:");
+         foreach(char ch in showIntersect)
+         {
+            Console.WriteLine(ch); 
+    if (ch == 'a')
+    {
+        total = total + 1;
+    }
+    else if (ch == 'b')
+    {
+        total = total + 2;
+    }
+     else if (ch == 'c')
+    {
+        total = total + 3;
+    }
+     else if (ch == 'd')
+    {
+        total = total + 4;
+    }
+     else if (ch == 'e')
+    {
+        total = total + 5;
+    }
+     else if (ch == 'f')
+    {
+        total = total + 6;
+    }
+     else if (ch == 'g')
+    {
+        total = total + 7;
+    }
+     else if (ch == 'h')
+    {
+        total = total + 8;
+    }
+     else if (ch == 'i')
+    {
+        total = total + 9;
+    }
+     else if (ch == 'j')
+    {
+        total = total + 10;
+    }
+     else if (ch == 'k')
+    {
+        total = total + 11;
+    }
+     else if (ch == 'l')
+    {
+        total = total + 12;
+    }
+     else if (ch == 'm')
+    {
+        total = total + 13;
+    }
+     else if (ch == 'n')
+    {
+        total = total + 14;
+    }
+     else if (ch == 'o')
+    {
+        total = total + 15;
+    }
+     else if (ch == 'p')
+    {
+        total = total + 16;
+    }
+     else if (ch == 'q')
+    {
+        total = total + 17;
+    }
+     else if (ch == 'r')
+    {
+        total = total + 18;
+    }
+     else if (ch == 's')
+    {
+        total = total + 19;
+    }
+     else if (ch == 't')
+    {
+        total = total + 20;
+    }
+      else if (ch == 'u')
+    {
+        total = total + 21;
+    }
+      else if (ch == 'v')
+    {
+        total = total + 22;
+    }
+      else if (ch == 'w')
+    {
+        total = total + 23;
+    }
+      else if (ch == 'x')
+    {
+        total = total + 24;
+    }
+      else if (ch == 'y')
+    {
+        total = total + 25;
+    }
+      else if (ch == 'z')
+    {
+        total = total + 26;
+    }
+       else if (ch == 'z')
+    {
+        total = total + 26;
+    }
+       else if (ch == 'A')
+    {
+        total = total + 27;
+    }
+       else if (ch == 'B')
+    {
+        total = total + 28;
+    }
+       else if (ch == 'C')
+    {
+        total = total + 29;
+    }
+       else if (ch == 'D')
+    {
+        total = total + 30;
+    }
+       else if (ch == 'E')
+    {
+        total = total + 31;
+    }
+       else if (ch == 'F')
+    {
+        total = total + 32;
+    }
+       else if (ch == 'G')
+    {
+        total = total + 33;
+    }
+       else if (ch == 'H')
+    {
+        total = total + 34;
+    }
+       else if (ch == 'I')
+    {
+        total = total + 35;
+    }
+       else if (ch == 'J')
+    {
+        total = total + 36;
+    }
+       else if (ch == 'K')
+    {
+        total = total + 37;
+    }
+       else if (ch == 'L')
+    {
+        total = total + 38;
+    }
+       else if (ch == 'M')
+    {
+        total = total + 39;
+    }
+       else if (ch == 'N')
+    {
+        total = total + 40;
+    }
+       else if (ch == 'O')
+    {
+        total = total + 41;
+    }
+       else if (ch == 'P')
+    {
+        total = total + 42;
+    }
+       else if (ch == 'Q')
+    {
+        total = total + 43;
+    }
+       else if (ch == 'R')
+    {
+        total = total + 44;
+    }
+       else if (ch == 'S')
+    {
+        total = total + 45;
+    }
+       else if (ch == 'T')
+    {
+        total = total + 46;
+    }
+       else if (ch == 'U')
+    {
+        total = total + 47;
+    }
+       else if (ch == 'V')
+    {
+        total = total + 48;
+    }
+       else if (ch == 'W')
+    {
+        total = total + 49;
+    }
+       else if (ch == 'X')
+    {
+        total = total + 50;
+    }
+       else if (ch == 'Y')
+    {
+        total = total + 51;
+    }
+       else if (ch == 'Z')
+    {
+        total = total + 52;
+    }
+   }
+   }
+   Console.WriteLine(total);
+
+
+
+
+   
+     
